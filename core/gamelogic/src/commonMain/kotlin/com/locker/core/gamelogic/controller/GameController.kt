e01d0da72121ada0f62bbf5b4f22f5b1ae665bd5
Locker <90060146+LockeRL@users.noreply.github.com>
2026-06-21 23:40:53 +0300

    - добавил отображение всех активных полей

--- core/gamelogic/src/commonMain/kotlin/com/locker/core/gamelogic/controller/GameController.kt
+++ core/gamelogic/src/commonMain/kotlin/com/locker/core/gamelogic/controller/GameController.kt
@@ -16,6 +16,10 @@ class GameController(
     private var _activeBlock: Pair<Int, Int>? = null
     val activeBlock: Pair<Int, Int>? get() = _activeBlock
 
+    init {
+	    setAllFieldActivity(true)
+    }
+
     fun onCellClick(fieldI: Int, fieldJ: Int, blockI: Int, blockJ: Int) {
         setFieldState(
             fieldI = fieldI,
@@ -29,6 +33,7 @@ class GameController(
         _activeBlock = null
         _activePlayer.value = Player.CROSS
         field.reset()
+	    setAllFieldActivity(false)
     }
 
     private fun setFieldState(fieldI: Int, fieldJ: Int, blockI: Int, blockJ: Int) {
@@ -39,7 +44,7 @@ class GameController(
             return
 
         val block = field[fieldI, fieldJ]
-        val setResult = block.setCellValue(blockI, blockJ, activePlayer.value)
+        val setResult = block.setCellValue(i = blockI, j = blockJ, state = activePlayer.value)
 
         if (setResult == SetCellResults.END_BLOCK) {
             val fieldRes = field.getAndUpdateBoardState()
@@ -51,7 +56,7 @@ class GameController(
 
         if (setResult == SetCellResults.SUCCESS || setResult == SetCellResults.END_BLOCK) {
             setActivePlayer()
-            setActiveBlock(blockI, blockJ)
+            setActiveBlock(i = blockI, j = blockJ)
         }
 
     }
@@ -66,9 +71,16 @@ class GameController(
         val block = field[i, j]
         if (block.winState.value != BoardState.InProgress) {
             _activeBlock = null
+            for (fieldI in 0 until field.dimension) {
+                for (fieldJ in 0 until field.dimension) {
+                    if (field[fieldI, fieldJ].winState.value == BoardState.InProgress) {
+                        field.setActiveFieldState(i = fieldI, j = fieldJ, state = true)
+                    }
+                }
+            }
         } else {
             _activeBlock = Pair(i, j)
-            field.setActiveFieldState(i, j, true)
+            field.setActiveFieldState(i = i, j = j, state = true)
         }
     }
 
@@ -78,12 +90,23 @@ class GameController(
     }
 
     private fun setPreviousBlockFalse() {
-        _activeBlock?.let { block ->
-            field.setActiveFieldState(block.first, block.second, false)
+        val activeBlock = _activeBlock
+        if (activeBlock != null) {
+            field.setActiveFieldState(i = activeBlock.first, j = activeBlock.second, state = false)
+        } else {
+	        setAllFieldActivity(false)
         }
     }
 
     private fun setActivePlayer() {
         _activePlayer.value = if (activePlayer.value == Player.CROSS) Player.CIRCLE else Player.CROSS
+    }
+
+    private fun setAllFieldActivity(isActive: Boolean) {
+	    for (i in 0 until field.dimension) {
+		    for (j in 0 until field.dimension) {
+			    field.setActiveFieldState(i = i, j = j, state = isActive)
+		    }
+	    }
     }
 }
