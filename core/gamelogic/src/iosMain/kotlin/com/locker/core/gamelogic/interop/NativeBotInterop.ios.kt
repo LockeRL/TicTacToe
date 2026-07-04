5809b17fa58fe9cb98614b6ff1d05733381e81e1
Locker <90060146+LockeRL@users.noreply.github.com>
2026-07-04 14:43:49 +0300

    исправил сборку на ios

--- core/gamelogic/src/iosMain/kotlin/com/locker/core/gamelogic/interop/NativeBotInterop.ios.kt
+++ core/gamelogic/src/iosMain/kotlin/com/locker/core/gamelogic/interop/NativeBotInterop.ios.kt
@@ -2,24 +2,28 @@ package com.locker.core.gamelogic.interop
 
 import com.locker.core.models.model.Move
 import com.locker.core.gamelogic.native.get_best_move
-import kotlinx.cinterop.*
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.toCValues
+import kotlinx.cinterop.useContents
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun getNativeMove(board: IntArray, macroBoard: IntArray, activeBlock: Int, player: Int, depth: Int): Move? {
-    memScoped {
-        val boardPtr = board.toCValues().getPointer(this)
-        val macroPtr = macroBoard.toCValues().getPointer(this)
-        
-        // Hardcoded 1000ms limit
-        val nativeMove = get_best_move(boardPtr, macroPtr, activeBlock, player, depth, 1000)
-        
-        if (nativeMove.fieldI == -1) return null
-        
-        return Move(
-            fieldI = nativeMove.fieldI,
-            fieldJ = nativeMove.fieldJ,
-            blockI = nativeMove.blockI,
-            blockJ = nativeMove.blockJ
-        )
-    }
+	memScoped {
+		val boardPtr = board.toCValues().getPointer(this)
+		val macroPtr = macroBoard.toCValues().getPointer(this)
+
+		// Hardcoded 1000ms limit
+		val nativeMove = get_best_move(boardPtr, macroPtr, activeBlock, player, depth, 1000)
+
+		return nativeMove.useContents {
+			if (fieldI == -1) null
+			else Move(
+				fieldI = fieldI,
+				fieldJ = fieldJ,
+				blockI = blockI,
+				blockJ = blockJ
+			)
+		}
+	}
 }
